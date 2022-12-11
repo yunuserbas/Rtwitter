732daeffbecd6a6a4f2797f5cad358f292aef6f9
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2022-12-11 15:04:21 +0100

    Update workshop.R

--- workshop.R
+++ workshop.R
@@ -1,5 +1,6 @@
 
 
+
 install.packages("rtweet")
 install.packages("tidyverse")
 
@@ -8,4 +9,11 @@ library(tidyverse)
 
 auth_setup_default()
 
-himars_data <- search_tweets("HIMARS", n = 250, include_rts = FALSE)
+himars_data <- search_tweets("HIMARS", n = 1500, include_rts = FALSE)
+
+himars_data_clean <- cbind(himars_data, users_data(himars_data)[,c("id", "id_str", "name", "screen_name")])
+
+himars_data_clean <- himars_data_clean[,!duplicated(colnames(himars_data_clean))]
+
+
+
